e5a83f973718b03ece7bb51ad4c806807e400e40
jzolotar <zolotarjakub@gmail.com>
2022-03-31 22:27:02 +0200

    add react-media package, set up different components to be render depending on current width for Navbar component

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,25 @@
 import { NavbarStyled } from './Navbar.styled';
 import NavbarDesktop from './NavbarDesktop/NavbarDesktop';
 import NavbarMobile from './NavbarMobile/NavbarMobile';
+import Media from 'react-media';
+import { Fragment } from 'react';
 
 const Navbar = () => {
-  return <NavbarDesktop />;
+  return (
+    <Media
+      queries={{
+        small: '(max-width: 650px)',
+
+        large: '(min-width: 1200px)',
+      }}
+    >
+      {(matches) => (
+        <Fragment>
+          {matches.small ? <NavbarMobile /> : <NavbarDesktop />}
+        </Fragment>
+      )}
+    </Media>
+  );
 };
 
 export default Navbar;
