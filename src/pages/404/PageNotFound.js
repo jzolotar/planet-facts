f393734753b98527438de5302ae626f22c274cb2
jzolotar <zolotarjakub@gmail.com>
2022-04-11 13:17:55 +0200

    add routing for gh-pages

--- src/pages/404/PageNotFound.js
+++ src/pages/404/PageNotFound.js
@@ -3,24 +3,25 @@ import { Link } from 'react-router-dom';
 
 const PageNotFound = () => {
   const pathsArr = [
-    '/mercury',
-    '/venus',
-    '/earth',
-    '/mars',
-    '/jupiter',
-    '/saturn',
-    '/uranus',
-    '/neptune',
+    '/planet-facts/mercury',
+    '/planet-facts/venus',
+    '/planet-facts/earth',
+    '/planet-facts/mars',
+    '/planet-facts/jupiter',
+    '/planet-facts/saturn',
+    '/planet-facts/uranus',
+    '/planet-facts/neptune',
   ];
 
   const randomPath = pathsArr[Math.floor(Math.random() * pathsArr.length)];
+  console.log(randomPath);
 
   return (
     <Styled404Page>
       <h1>404</h1>
       <p>
         Oops, seems like this page doesn't exist. But don't worry you can always
-        check other pages to learn some cool thgins about our planets.
+        check other pages to learn some cool things about planets.
       </p>
       <Link to={randomPath}>Random Planet</Link>
     </Styled404Page>
